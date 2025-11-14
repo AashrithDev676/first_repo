b30791f185082af62be70266450dd39be8b93759
Aashrith <pendyalaaashrith@gmail.com>
2025-11-14 20:16:28 +0530

    Circle class is added

---
+++ Circle.java
@@ -0,0 +1,11 @@
+class Circle {
+    private double radius;
+
+    public Circle(double radius){
+        this.radius=radius;
+    }
+
+    public double area(){
+        return 3.14*radius*radius;
+    }
+}
