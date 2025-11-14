6d01703bef9b3921da4f7cd5a20cf637c2eb3494
Aashrith <pendyalaaashrith@gmail.com>
2025-11-14 20:25:09 +0530

    Implemented circle class in Main file

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Welcome to Git");
+        Circle circle=new Circle(4.56);
+        System.out.println("Area : "+circle.area());
+        System.out.println("Circumference : "+ circle.circumference());
     }
 }
